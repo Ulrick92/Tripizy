952c704d916852389a6139ef1d581a97490b7244
Tomoko ISHIDA <ti@macbook-pro-de-tomoko.home>
2018-12-09 20:36:33 +0100

    first commit

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -29,10 +29,12 @@ import { createSwitchNavigator } from "react-navigation";
 
 import AuthStackNavigator from "./AuthStackNavigator";
 import MainTabNavigator from "./MainTabNavigator";
+import FormStackNavigator from "./FormStackNavigator";
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Auth: AuthStackNavigator, // Tomoko
-  Main: MainTabNavigator // Laurent
+  Main: MainTabNavigator, // Laurent
+  Form: FormStackNavigator
 });
