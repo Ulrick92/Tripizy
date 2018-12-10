109ccb3beec1465dec3c3ab2a3fba265f45297ac
LaurentBonnec <laurent.bonnec@gmail.com>
2018-12-10 19:27:22 +0100

    debut stepcard

--- screens/ListScreen/List.js
+++ screens/ListScreen/List.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text
+} from "react-native";
 import TravelBookCard from "../../components/TravelBookCard";
+import axios from "axios";
 
 export default class ListScreen extends React.Component {
   static navigationOptions = {
@@ -9,6 +16,10 @@ export default class ListScreen extends React.Component {
       backgroundColor: "#37449E"
     },
     headerTintColor: "#fff"
+  };
+
+  state = {
+    first_name: ""
   };
 
   render() {
@@ -27,6 +38,17 @@ export default class ListScreen extends React.Component {
         <TravelBookCard />
         <TravelBookCard />
         <TravelBookCard />
+        <TouchableOpacity style={styles.button}>
+          <Text
+            style={styles.buttonText}
+            title="Go to Create a Travel Book"
+            // onPress={() =>
+            //   this.props.navigation.navigate("TitleAndDescription")
+            // }
+          >
+            +
+          </Text>
+        </TouchableOpacity>
       </ScrollView>
     );
   }
@@ -35,6 +57,23 @@ export default class ListScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#EAE1E2"
+  },
+  button: {
+    alignSelf: "flex-end",
+    borderColor: "rgba(0,0,0,0.2)",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+    backgroundColor: "#fff",
+    borderRadius: 100,
+    position: "absolute",
+    right: 10
+
+    // marginBottom: 150
+  },
+  buttonText: {
+    fontSize: 30
   }
 });
