74fa5953f76663ff158d406182f4f5e1383031df
Tomoko ISHIDA <ti@mbp-de-tomoko.lan>
2018-12-12 19:48:10 +0100

    first commit

--- screens/TravelBookFormScreen/Category.js
+++ screens/TravelBookFormScreen/Category.js
@@ -5,9 +5,13 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   AsyncStorage,
+  Icon,
   Image,
   StyleSheet
 } from "react-native";
+import { CheckBox } from "react-native-elements";
+import "@expo/vector-icons";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 // import styles from "./styles";
 import axios from "axios";
 
@@ -26,30 +30,40 @@ export default class Category extends Component {
   };
 
   state = {
-    category: "Solo"
+    backpack: false,
+    family: false,
+    work: false,
+    comfort: false,
+    worldTour: false,
+    cruising: false,
+    nature: false,
+    roadTrip: false
   };
 
   redirectToLoginPage = () => {
     this.props.history.push("/log_in");
   };
 
-  handlePress = () => {
-    this.setState({
-      category: value
-    });
-  };
-
   handleSubmit = event => {
-    const { category } = this.state;
-
     AsyncStorage.getItem("token", (err, token) => {
       console.log("result", token);
-      console.log("title", this.props.navigation.state.params.title);
-      console.log("country", this.props.navigation.state.params.country);
-      console.log("city", this.props.navigation.state.params.city);
-      console.log("start_date", this.props.navigation.state.params.start_date);
-      console.log("end_date", this.props.navigation.state.params.end_date);
-      console.log("photos", this.props.navigation.state.params.photos);
+
+      categoriesToString = () => {
+        console.log(this.state);
+        const categoriesArray = [];
+        const keysArray = Object.keys(this.state);
+        const valuesArray = Object.values(this.state);
+
+        for (let i = 0; i < keysArray.length; i++) {
+          if (valuesArray[i] === true) {
+            categoriesArray.push(keysArray[i]);
+          }
+        }
+        return categoriesArray;
+      };
+
+      const valeur = categoriesToString();
+      console.log(valeur); // va afficher categoriesArray
 
       if (!token) {
         this.redirectToLoginPage();
@@ -66,7 +80,7 @@ export default class Category extends Component {
               ),
               end_date: Date.parse(this.props.navigation.state.params.end_date),
               photos: this.props.navigation.state.params.photos,
-              category
+              category: valeur
             },
             {
               headers: {
@@ -74,9 +88,11 @@ export default class Category extends Component {
               }
             }
           )
+
           .then(response => {
             console.log("response", response.data);
-            this.props.navigation.navigate("LogIn", {
+
+            this.props.navigation.navigate("TravelBookCard", {
               title: response.data.title,
               description: response.data.description,
               country: response.data.country,
@@ -97,105 +113,162 @@ export default class Category extends Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <Text style={styles.title}>CREATE A TRAVEL BOOK</Text>
-        <Text style={styles.hint}>What is your travelling style ?</Text>
-        {/* <Image
-          style={styles.icon} */}
-        {/* //   source={{ uri: this.state.user.travelbooks[i].photos[0] }} */}
-        {/* <TextInput
-          style={styles.input}
-          autoCapitalize="none"
-          value={this.state.category}
-          placeholder={"ex : Select categories"}
-          onChangeText={value => {
-            this.setState({
-              category: value
-            });
-          }}
-        /> */}
-        <View style={styles.buttonRow}>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="backpacker"
-              value={this.state.category}
-              source={require("../../assets/icons/backpack-50.png")}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="family"
-              value={this.state.category}
-              source={require("../../assets/icons/family-50.png")}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="business"
-              value={this.state.category}
-              source={require("../../assets/icons/suitcase-50.png")}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="luxe"
-              value={this.state.category}
-              source={require("../../assets/icons/diamond-50.png")}
-            />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.title}>CREATE A TRAVEL BOOK</Text>
+          <Text style={styles.hint}>What is your travelling style ?</Text>
+          <View style={styles.buttonRow}>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/backpack-filled-50.png")}
+              />
+              {/* <FontAwesomeIcon name=“hotel” size={50} color=“black” /> */}
+              <Text style={styles.label}>Backpack</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                style={styles.checkBox}
+                checked={this.state.backpack}
+                onPress={() =>
+                  this.setState({ backpack: !this.state.backpack })
+                }
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/family-filled-50.png")}
+              />
+              <Text style={styles.label}>Family</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.family}
+                onPress={() => this.setState({ family: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/suitcase-filled-50.png")}
+              />
+              <Text style={styles.label}>Work</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.work}
+                onPress={() => this.setState({ work: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/diamond-filled-50.png")}
+              />
+              <Text style={styles.label}>Comfort</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.comfort}
+                onPress={() => this.setState({ comfort: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+          </View>
+          <View style={styles.buttonRow}>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/europe-filled-50.png")}
+              />
+              <Text style={styles.label}>World Tour</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.worldTour}
+                onPress={() => this.setState({ worldTour: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/cruise-ship-filled-50.png")}
+              />
+              <Text style={styles.label}>Cruising</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.cruising}
+                onPress={() => this.setState({ cruising: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/national-park-filled-50.png")}
+              />
+              <Text style={styles.label}>Nature</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.nature}
+                onPress={() => this.setState({ nature: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+            <View style={styles.buttonIcon}>
+              <Image
+                style={{ tintColor: "white" }}
+                source={require("../../assets/icons/road-filled-50.png")}
+              />
+              <Text style={styles.label}>Road Trip</Text>
+              <CheckBox
+                textStyle={styles.textStyle}
+                containerStyle={styles.containerStyle}
+                center={true}
+                style={styles.checkBox}
+                checked={this.state.roadTrip}
+                onPress={() => this.setState({ roadTrip: true })}
+                // onPress={() => this.setState(this.handlePress)}
+              />
+            </View>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={this.handleSubmit}>
+            <Text style={styles.buttonText}>SUBMIT</Text>
           </TouchableOpacity>
         </View>
-        <View style={styles.buttonRow}>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="world-tour"
-              value={this.state.category}
-              source={require("../../assets/icons/europe-filled-50.png")}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="cruising"
-              value={this.state.category}
-              source={require("../../assets/icons/cruise-ship-filled-50.png")}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="nature"
-              value={this.state.category}
-              source={require("../../assets/icons/national-park-filled-50.png")}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.handlePress}>
-            <Image
-              style={styles.buttonIcon}
-              title="road-trip"
-              value={this.state.category}
-              source={require("../../assets/icons/road-filled-50.png")}
-            />
-          </TouchableOpacity>
-        </View>
-
-        <TouchableOpacity style={styles.button} onPress={this.handleSubmit}>
-          <Text style={styles.buttonText}>SUBMIT</Text>
-        </TouchableOpacity>
       </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0040cc"
+  },
   title: {
     textAlign: "center",
     fontSize: 30,
     color: "white",
-    marginBottom: 30,
+    marginVertical: 30,
     fontWeight: "200"
   },
   hint: {
@@ -205,41 +278,46 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontWeight: "200"
   },
-  container: {
+  buttonContainer: {
     flex: 1,
-    backgroundColor: "#0040cc",
     justifyContent: "center",
-    alignItems: "center"
-  },
-  input: {
-    width: 250,
-    height: 60,
-    color: "white",
-    borderColor: "white",
-    borderBottomWidth: 1,
-    paddingLeft: 10,
     alignItems: "center"
   },
   buttonRow: {
     flexDirection: "row",
-    display: "flex",
-    marginVertical: 20
+    marginVertical: 30,
+    justifyContent: "space-around"
   },
   buttonIcon: {
-    height: 30,
-    width: 30
+    flexDirection: "column",
+    alignItems: "center"
   },
+  label: {
+    color: "white",
+    marginVertical: 10
+  },
+  checkBox: {},
   button: {
     marginTop: 20,
     backgroundColor: "rgba(255, 255, 255, 0.3)",
     height: 50,
     width: 250,
     justifyContent: "center",
+    alignItems: "center",
     borderColor: "white",
     borderRadius: 5
   },
   buttonText: {
     color: "white",
     textAlign: "center"
+  },
+  textStyle: {
+    display: "none"
+  },
+  containerStyle: {
+    width: 60,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center"
   }
 });
