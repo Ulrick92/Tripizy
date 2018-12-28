a43e3c32e429a5791c88a714999b56676491de7c
zizou <zizouvb@users.noreply.github.com>
2018-12-28 12:37:34 +0100

    hide/show addtip button && display step with calendar picker

--- components/StepCard.js
+++ components/StepCard.js
@@ -23,15 +23,51 @@ class StepCard extends React.Component {
           console.log("StepState => ", response.data);
           this.setState({
             step: response.data,
-            mounted: true,
-            tips: response.data.tips
+
+            tips: response.data.tips,
+            travelBookUserId: response.data.travelbook_id.user_id
           });
+          axios
+            .get(`${config.DOMAIN}user`, {
+              headers: {
+                authorization: `Bearer ${token}`
+              }
+            })
+            .then(response => {
+              console.log("User => ", response.data);
+              this.setState({
+                userId: response.data._id,
+                mounted: true
+              });
+            })
+            .catch(err => {
+              console.log("get user id", err.message);
+            });
         })
         .catch(err => {
-          console.log(err);
+          console.log("get step", err.message);
         });
     });
   }
+  renderAddTipsButton = () => {
+    // hide "+ tip" button if user is not the owner of travelbook
+    if (this.state.mounted && this.state.travelBookUserId === this.state.userId)
+      return (
+        <TouchableOpacity
+          onPress={() => {
+            this.props.navigation.navigate("TipsForm", {
+              stepId: this.state.step._id
+            });
+          }}
+        >
+          <View style={{ justifyContent: "center", marginEnd: 10 }}>
+            <MaterialIconsIcon name="add-circle" />
+            <Text>Add tip</Text>
+          </View>
+        </TouchableOpacity>
+      );
+    else return null;
+  };
   render() {
     const { mounted, step } = this.state;
     const { index } = this.props;
@@ -70,18 +106,7 @@ class StepCard extends React.Component {
           >
             {date.toDateString()}
           </Text>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.navigation.navigate("TipsForm", {
-                stepId: this.state.step._id
-              });
-            }}
-          >
-            <View style={{ justifyContent: "center", marginEnd: 10 }}>
-              <MaterialIconsIcon name="add-circle" />
-              <Text>Add tip</Text>
-            </View>
-          </TouchableOpacity>
+          {this.renderAddTipsButton()}
         </View>
       );
     } else {
